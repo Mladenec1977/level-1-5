1dab6776b3be4abcdfdbb7f7649fb3926f7ab555
Mladenec1977 <rozgon1977@gmail.com>
2021-06-03 23:05:36 +0300

    add eslint,prettier,husky

--- gulpfile.js
+++ gulpfile.js
@@ -6,31 +6,31 @@ const sourcemaps = require("gulp-sourcemaps");
 const buffer = require("vinyl-buffer");
 const moment = require("moment");
 const paths = {
-    pages: ["src/*.html", "src/*.css"],
+  pages: ["src/*.html", "src/*.css"],
 };
 gulp.task("copy-html", function () {
-    return gulp.src(paths.pages).pipe(gulp.dest("dist"));
+  return gulp.src(paths.pages).pipe(gulp.dest("dist"));
 });
 gulp.task(
-    "default",
-    gulp.series(gulp.parallel("copy-html"), function () {
-        return browserify({
-            basedir: ".",
-            debug: true,
-            entries: ["src/main.ts"],
-            cache: {},
-            packageCache: {},
-        })
-            .plugin(tsify)
-            .transform("babelify", {
-                presets: ["es2015"],
-                extensions: [".ts"],
-            })
-            .bundle()
-            .pipe(source("bundle.js"))
-            .pipe(buffer())
-            .pipe(sourcemaps.init({ loadMaps: true }))
-            .pipe(sourcemaps.write("./"))
-            .pipe(gulp.dest("dist"));
+  "default",
+  gulp.series(gulp.parallel("copy-html"), function () {
+    return browserify({
+      basedir: ".",
+      debug: true,
+      entries: ["src/main.ts"],
+      cache: {},
+      packageCache: {},
     })
+      .plugin(tsify)
+      .transform("babelify", {
+        presets: ["es2015"],
+        extensions: [".ts"],
+      })
+      .bundle()
+      .pipe(source("bundle.js"))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write("./"))
+      .pipe(gulp.dest("dist"));
+  })
 );
